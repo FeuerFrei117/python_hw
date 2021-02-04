2de4a6a0e7702c5c590c1ef1a1c2629b89b3b725
Roman <Chepkinrv@mail.ru>
2021-02-04 23:29:39 +0700

    add hw_2

---
+++ hw_2/EX_1.py
@@ -0,0 +1,20 @@
+# Поработайте с переменными, создайте несколько, выведите на экран, запросите у пользователя несколько чисел и строк и
+# сохраните в переменные, выведите на экран.
+
+number_one = 17
+print(number_one)
+
+number_two = 'семнадцать'
+print(number_two)
+
+number_tree = 17.7
+print(number_tree)
+
+number_four = int(input('Введите целове число: '))
+print(number_four)
+
+number_five = float(input('Введите число с плавающей точкой: '))
+print(number_five)
+
+number_six = input('Введите одно слово: ')
+print(number_six)
